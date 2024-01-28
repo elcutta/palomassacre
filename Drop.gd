eca51609a3c2fd78586e91da27a14c6d43318438
Ariel Cattaneo <ariel.cattaneo@gmail.com>
2024-01-28 10:59:03 -0300

    Drop texture and dynamic size while falling

--- Drop.gd
+++ Drop.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-var height = 100
+const MAX_HEIGHT = 100
+var height = MAX_HEIGHT
 const FALLING_SPEED = 50
+const SCALE_MAX = 1.1
+const SCALE_MIN = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	height = height - (FALLING_SPEED * delta)
+	var newScale = (height / MAX_HEIGHT) * (SCALE_MAX - SCALE_MIN) + SCALE_MIN
+	$DropSprite.scale = Vector2(newScale, newScale)
 	if (height <= 0):
 		queue_free()
